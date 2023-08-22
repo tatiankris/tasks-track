e657b7d4deb6965f03f83bedea66c8cd26fd7fa1
tatiankris <tatiankris@mail.ru>
2023-08-22 16:59:03 +0300

    update backend url

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const baseURL = 'http://localhost:5000'
+export const baseURL = 'https://task-track-back.vercel.app' //http://localhost:5000'
 
 export const instance = axios.create({
   baseURL: baseURL,
